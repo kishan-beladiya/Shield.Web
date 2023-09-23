2acaf79074db7e70393131120f1d5cbc2f2c39e3
kishan-beladiya <kishanbeladiya925@gmail.com>
2023-09-23 20:27:36 +0530

    Updates

---
+++ precache-manifest.0e14a5ec20e3e1a803f7cbe9858d2a10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8043c9e38aae15d581eb4d09ad4ea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec6905cb33a836ffb126",
+    "url": "/static/css/main.e440ba25.chunk.css"
+  },
+  {
+    "revision": "8268a0bb945486ecef3e",
+    "url": "/static/js/2.29a17a2c.chunk.js"
+  },
+  {
+    "revision": "ec6905cb33a836ffb126",
+    "url": "/static/js/main.12bd8fe6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
